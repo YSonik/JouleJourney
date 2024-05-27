9daf5954ee92d5a7ae03b547979b99aefd297a7e
Yogesh Sonik <yogesh.sonik135@gmail.com>
2024-05-27 01:00:37 -0700

    Created search component for initiating searches.

---
+++ Front_End/src/app/journey-search/journey-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journey-search',
+  templateUrl: './journey-search.component.html',
+  styleUrl: './journey-search.component.css'
+})
+export class JourneySearchComponent {
+
+}
