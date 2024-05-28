44d14c6ea3e94ffd087881c63a636dd63322b1ba
Yogesh Sonik <yogesh.sonik135@gmail.com>
2024-05-27 20:40:54 -0700

    Implemented event based communication between two the map and search components.

--- Front_End/src/app/map/journey-search/journey-search.component.ts
+++ Front_End/src/app/map/journey-search/journey-search.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+
+type journey_data = {
+  origin_string: string;
+  destination_string: string;
+  range_string: string;
+}
 
 @Component({
   selector: 'app-journey-search',
   templateUrl: './journey-search.component.html',
   styleUrl: './journey-search.component.css'
 })
-export class JourneySearchComponent {
+export class JourneySearchComponent implements OnInit{
+  @Output() create_journey_event = new EventEmitter<journey_data>(); 
+  #origin_element!:HTMLInputElement;
+  #destination_element!:HTMLInputElement;
+  #range_element!:HTMLInputElement;
 
-
-  create_journey()
+  request_journey()
   {
+    //Extract the values from the three input fields.
+    let data: journey_data = {
+      origin_string: this.#origin_element.value,
+      destination_string: this.#destination_element.value,
+      range_string: this.#range_element.value
+    };
+
     //Emit the create_journey event to the map component.
-    
+    this.create_journey_event.emit(data);
   }
+
+  ngOnInit()
+  {
+    this.#origin_element = (document.getElementById("origin_input") as HTMLInputElement);
+    this.#destination_element = (document.getElementById("destination_input") as HTMLInputElement);
+    this.#range_element = (document.getElementById("range_input") as HTMLInputElement);
+  }
+
 }
--- Front_End/src/app/map/map.component.ts
+++ Front_End/src/app/map/map.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleMapsService } from '../services/google-maps.service';
 
+type journey_data = {
+  origin_string: string;
+  destination_string: string;
+  range_string: string;
+}
+
 @Component({
   selector: 'embedded-map',
   templateUrl: './map.component.html',
@@ -11,17 +17,18 @@ export class MapComponent implements OnInit {
   //Member Variables
   #map_api_key!: string;
   #map_options!: google.maps.MapOptions;
+  #map_options_ready: boolean = false;
+
   #map_element!: any;
   #map_object!: google.maps.Map;
 
+
   #renderMap()
   {
-    //Render a map centered at Seattle.
-    this.#map_options = {
-      center: {lat: 47.608013, lng: -122.335167},
-      zoom: 11,
-      scrollwheel: false
-    };
+    while(!this.#map_options_ready)
+    {
+      //Busy wait until user's coordinates are being fetched.
+    }
     this.#map_element = document.getElementById("embedded_map");
     this.#map_object = new google.maps.Map(this.#map_element, this.#map_options);
   }
@@ -39,12 +46,43 @@ export class MapComponent implements OnInit {
   ngOnInit()
   {
     this.#map_api_key = this.googleMapsService.getApiKey();
-    this.#loadScript();
+    
+    //Center the map at the user's location or at Seattle.
+    if(navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition((position)=>
+      {
+        this.#map_options = {
+          center: {lat: position.coords.latitude, lng: position.coords.longitude},
+          zoom: 11,
+          scrollwheel: false,
+          mapTypeControl: false
+        }
+        this.#map_options_ready = true;
+      });
+    }
+    else
+    {
+      this.#map_options = {
+        center: {lat: 47.608013, lng: -122.335167},
+        zoom: 11,
+        scrollwheel: false,
+        mapTypeControl: false
+      };
+      this.#map_options_ready = true;
+    }
+    
+    //this.#loadScript();
   }
 
   constructor(private googleMapsService: GoogleMapsService)
   {
     //Initialization work done in ngOnInit lifecycle hook.
+  }
+
+  create_journey(data: journey_data)
+  {
+    console.log(data);
   }
 
 }
